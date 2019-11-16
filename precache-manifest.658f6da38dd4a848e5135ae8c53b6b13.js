d078a1954b18f8042282e3b5192eb92fedc46307
bellakocharyan <bellak1985@gmail.com>
2019-11-16 14:13:11 +0400

    Updates

---
+++ precache-manifest.658f6da38dd4a848e5135ae8c53b6b13.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626440e5b07ab7e37ba2d871436517da",
+    "url": "/Team-Project/public/index.html"
+  },
+  {
+    "revision": "ecefe2a4a4657c4c2579",
+    "url": "/Team-Project/public/static/css/4.228ab2f0.chunk.css"
+  },
+  {
+    "revision": "babf5be0bc96abaf1b02",
+    "url": "/Team-Project/public/static/css/main.780a1ba8.chunk.css"
+  },
+  {
+    "revision": "85de11ee149ad5c013e9",
+    "url": "/Team-Project/public/static/js/2.48b3d0a1.chunk.js"
+  },
+  {
+    "revision": "e785047b74033d6b7eff",
+    "url": "/Team-Project/public/static/js/3.53c8a004.chunk.js"
+  },
+  {
+    "revision": "ecefe2a4a4657c4c2579",
+    "url": "/Team-Project/public/static/js/4.151cf6bf.chunk.js"
+  },
+  {
+    "revision": "a9cb5adfaacaa45e2e94",
+    "url": "/Team-Project/public/static/js/5.0e170526.chunk.js"
+  },
+  {
+    "revision": "dc8893daa95ca0542300",
+    "url": "/Team-Project/public/static/js/6.84229f47.chunk.js"
+  },
+  {
+    "revision": "97795a78e68a46623aef",
+    "url": "/Team-Project/public/static/js/7.559499cf.chunk.js"
+  },
+  {
+    "revision": "8866cd6769ac77cb28fc",
+    "url": "/Team-Project/public/static/js/8.1c2ec057.chunk.js"
+  },
+  {
+    "revision": "babf5be0bc96abaf1b02",
+    "url": "/Team-Project/public/static/js/main.0c12d4bb.chunk.js"
+  },
+  {
+    "revision": "37f54607886ae9c489c9",
+    "url": "/Team-Project/public/static/js/runtime-main.1299d593.js"
+  },
+  {
+    "revision": "da6ec07e9655df7bbb4b23ca82880d3e",
+    "url": "/Team-Project/public/static/media/logo.da6ec07e.png"
+  },
+  {
+    "revision": "67137dcaf8fb3d6d2cd17203f20d81b4",
+    "url": "/Team-Project/public/static/media/slide_1.67137dca.jpg"
+  },
+  {
+    "revision": "d27075813d4f284ff6529f08dfc08769",
+    "url": "/Team-Project/public/static/media/slide_2.d2707581.jpg"
+  },
+  {
+    "revision": "0736f682042aa0e40f3cfa2bc462d689",
+    "url": "/Team-Project/public/static/media/slide_3.0736f682.jpg"
+  }
+]);
